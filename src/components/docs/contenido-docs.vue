7f8b16ef4e739932a5d6b309db5fcbacd8972e64
Araozu (woo) <faraoz@unsa.edu.pe>
2020-06-14 14:36:09 -0500

    Agregado codigo para que los links que llevan a otras partes de la pagina en la documentacion usen el vue-router.

--- src/components/docs/contenido-docs.vue
+++ src/components/docs/contenido-docs.vue
@@ -6,7 +6,20 @@ div(v-html="mdhtml" ref="elem")
 
 <script lang="coffee">
     import { ref, onMounted } from "vue"
+    import { useRouter } from "vue-router"
     import { kanAHtml } from "../codigo/kanAHTML.coffee"
+
+    origin = window.location.origin
+
+    actualizarElementosA = (elemHtml, router) =>
+        elementos = elemHtml.querySelectorAll "a"
+        for elemento in elementos
+            if elemento.href.startsWith (origin + "/#/")
+                elemento.addEventListener "click", (ev) =>
+                    ev.preventDefault();
+                    nuevaDir = elemento.href.substring (origin.length + 2)
+                    router.push nuevaDir
+
 
     actualizarElementosCode = (elemHtml) =>
         elementos = elemHtml.querySelectorAll "code"
@@ -31,8 +44,12 @@ div(v-html="mdhtml" ref="elem")
                 required: true
         setup: (props) =>
             elem = ref null
+            router = useRouter()
 
-            onMounted (=> actualizarElementosCode elem.value)
+            onMounted (=>
+                actualizarElementosCode elem.value
+                actualizarElementosA elem.value, router
+            )
 
             {
                 elem
